506d361c316b58ff06f1a809b364608d6656c267
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-06-30 23:56:51 -0700

    update homepage

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,21 @@
-var App = function(p5js) {
+var App = function (p5js) {
   var iframe;
 
   p5js.setup = function () {
     p5js.noCanvas();
 
     createMenu();
-    highlightMenu(p5js.select('li'))
+    highlightMenu(p5js.select("li"));
 
     initialize(Demos.examples[0]);
 
-    var items = p5js.selectAll('li');
+    var items = p5js.selectAll("li");
     for (var i = 0; i < items.length; i++) {
       items[i].mouseClicked(updateExample);
     }
-  }
+  };
 
-  function initialize(initialExample){
+  function initialize(initialExample) {
     iframe = createIframe(initialExample);
 
     setExampleText(initialExample);
@@ -23,41 +23,61 @@ var App = function(p5js) {
 
   function createMenu() {
     Demos.examples.forEach(function (example) {
-      p5js.createElement("li", example.name)
+      p5js
+        .createElement("li", example.title)
+        .attribute("data-link", example.name)
         .parent("links");
     });
   }
 
   function createIframe(initialExample) {
-    return p5js.select('#demo-frame')
-      .attribute('width', '600px')
-      .attribute('height', '400px')
-      .attribute('src',  Demos.createIndexUrl(initialExample.name));
+    return p5js
+      .select("#demo-frame")
+      .attribute("width", "600px")
+      .attribute("height", "400px")
+      .attribute("src", Demos.createIndexUrl(initialExample.name));
   }
 
   function setExampleText(example) {
-    p5js.select('#description')
-      .html(example.description);
+    p5js.select("#title").html(example.title);
 
-    p5js.select('#instructions')
-      .html(example.instructions);
+    p5js.select("#description").html(example.description);
 
-      p5js.select('#source')
-        .html('<a href="' + Demos.createSourceUrl(example.name)+ '">Source</a>');
+    if (example.instructions) {
+      p5js
+        .select("#instructions")
+        .html("<h2>Instructions</h2><p>" + example.instructions + "</p>");
+    } else {
+      p5js.select("#instructions").html("");
+    }
+
+    if (example.youtube_link) {
+      p5js
+        .select("#youtube-link")
+        .html('<a href="' + example.youtube_link + '">YouTube link</a>');
+    } else {
+      p5js.select("#youtube-link").html("");
+    }
+
+    p5js
+      .select("#source")
+      .html(
+        '<a href="' + Demos.createSourceUrl(example.name) + '">Source Code</a>'
+      );
   }
 
   function updateExample() {
     highlightMenu(this);
-    iframe.attribute('src', Demos.createIndexUrl(this.elt.innerHTML));
-    setExampleText(Demos.findExample(this.elt.innerHTML));
+    iframe.attribute("src", Demos.createIndexUrl(this.elt.dataset.link));
+    setExampleText(Demos.findExample(this.elt.dataset.link));
   }
 
-  function highlightMenu(menuItem){
-    var current = document.querySelector('li.current')
-    if(current){
-      current.removeAttribute('class');
+  function highlightMenu(menuItem) {
+    var current = document.querySelector("li.current");
+    if (current) {
+      current.removeAttribute("class");
     }
-    menuItem.attribute('class', 'current');
+    menuItem.attribute("class", "current");
   }
 };
 
